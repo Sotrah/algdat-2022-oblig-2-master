79e4a771aa1939a21f20a90b4ccd18d387904ddf
jonasolssson <s364699@oslomet.no>
2022-10-05 17:12:54 +0200

    implementert oppg1.
    Gjort all implementasjon til oppgave 1. kan først testes når oppgave 2 er ferdigstilt. mulig det er noen bugs som kommer frem når 2 er ferdig men ellers good to go!

--- src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
@@ -56,6 +56,30 @@ public class DobbeltLenketListe<T> implements Liste<T> {
         på a.
         * hvis den neste verdien i a ikke er null opprettes en ny node, og antall økes med 1.
         */
+
+        if (a == null) throw new NullPointerException("Lista er tom");
+
+        if (a.length > 0){
+            int i = 0;
+            for(;i < a.length; i++){
+                if(a[i] != null) {
+                    hode = new Node<>(a[i]);
+                    antall++;
+                    break;
+                }
+            }
+            hale = hode;
+            if (hode != null) {
+                i++;
+                for (; i < a.length; i++) {
+                    if (a[i] != null) {
+                        hale.neste = new Node<>(a[i], hale, null);
+                        hale = hale.neste;
+                        antall++;
+                    }
+                }
+            }
+        }
     }
 
     public Liste<T> subliste(int fra, int til) {
@@ -65,9 +89,7 @@ public class DobbeltLenketListe<T> implements Liste<T> {
     @Override
     // Jonas
     public int antall() {
-        // Første metode i oppgave 1
-        // kun return statement. heltallet behandles i konstruktør.
-
+        return antall;
     }
 
     @Override
@@ -79,6 +101,8 @@ public class DobbeltLenketListe<T> implements Liste<T> {
         er null skal det returneres true da hodet
         hadde hatt noe annet enn nullverdi om lista ikke var tom.
         */
+        if (hode == null) return true;
+        else return false;
     }
 
     @Override
